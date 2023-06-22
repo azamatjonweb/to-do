e7111d92a99ca969b253e01e705dec0592cdc168
AzamatjonWeb <130630383+AzamatjonWeb@users.noreply.github.com>
2023-06-22 09:32:08 +0500

    js-edit

--- js/script.js
+++ js/script.js
@@ -10,27 +10,31 @@ filterOption.addEventListener("change", filterTodo);
 
 function addTodo(event) {
     event.preventDefault();
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-    const newTodo = document.createElement("li");
-    newTodo.innerText = todoInput.value; 
-    newTodo.classList.add("todo-item");
-    todoDiv.appendChild(newTodo);
-    //ADDING TO LOCAL STORAGE 
-    saveLocalTodos(todoInput.value);
-    
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="fas fa-check"></li>';
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-
-    const trashButton = document.createElement("button");
-    trashButton.innerHTML = '<i class="fas fa-trash"></li>';
-    trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
-    
-    todoList.appendChild(todoDiv);
-    todoInput.value = "";
+    if(todoInput.value === "") {
+        alert("Write the To-Do!");
+    } else {
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todoInput.value; 
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+        //ADDING TO LOCAL STORAGE 
+        saveLocalTodos(todoInput.value);
+        
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = '<i class="fas fa-check"></li>';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+        
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = '<i class="fas fa-trash"></li>';
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+        
+        todoList.appendChild(todoDiv);
+        todoInput.value = "";
+    }
 }
 
 function deleteCheck(e) {
